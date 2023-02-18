dd5eec2f8768fe1b82542d1e0963a9e33e8f9622
trykov <yuri.trykov@indriver.com>
2023-02-18 17:44:28 +0200

    Demo project.pbxproj

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Yuri Trykov on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
